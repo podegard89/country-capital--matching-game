b6b8a175eeeeb314d3406474597fd6de11fe838c
Pearce Odegard <pearceodegard@gmail.com>
2023-09-27 15:34:35 -0500

    Completed all instructions, added instructions PNG

--- src/components/CountryCapitalGame.tsx
+++ src/components/CountryCapitalGame.tsx
@@ -1,3 +1,86 @@
-export default function CountryCapitalGame() {
-  return <div>CountryCapitalGame</div>;
+import { useState } from "react";
+import styles from "./CountryCapitalGame.module.css";
+
+// each key is a country and the value is a capital
+
+interface CountryCapitalGameProps {
+  data: Record<string, string>;
+}
+
+type Option = {
+  value: string;
+  state: "default" | "selected" | "incorrect";
+};
+
+export default function CountryCapitalGame({ data }: CountryCapitalGameProps) {
+  const countryCapitalArray = objectToRandomArray(data);
+  const [options, setOptions] = useState<Option[]>(
+    countryCapitalArray.map((item) => ({ value: item, state: "default" }))
+  );
+
+  const [selected, setSelected] = useState<string>();
+
+  const buttonClickHandler = (opt: Option) => {
+    if (!selected) {
+      setOptions(
+        options.map((item) => {
+          return item.value === opt.value
+            ? { ...item, state: "selected" }
+            : { ...item, state: "default" };
+        })
+      );
+      setSelected(opt.value);
+    } else if (
+      data[selected ?? ""] === opt.value ||
+      selected === data[opt.value]
+    ) {
+      setOptions(
+        options.filter((item) => {
+          return !(item.value === opt.value || item.value === selected);
+        })
+      );
+      setSelected(undefined);
+    } else {
+      setOptions(
+        options.map((item) => {
+          return item.value === opt.value || item.value === selected
+            ? { ...item, state: "incorrect" }
+            : item;
+        })
+      );
+      setSelected(undefined);
+    }
+  };
+
+  return (
+    <div>
+      {options.map((opt) => {
+        return (
+          <button
+            key={opt.value}
+            onClick={() => buttonClickHandler(opt)}
+            className={styles[opt.state]}
+          >
+            {opt.value}
+          </button>
+        );
+      })}
+      {options.length === 0 && <p>Congratulations!</p>}
+    </div>
+  );
+}
+
+function objectToRandomArray(obj: { [key: string]: string }): string[] {
+  const resultArray = [...Object.values(obj), ...Object.keys(obj)];
+
+  shuffleArray(resultArray);
+
+  return resultArray;
+}
+
+function shuffleArray(arr: string[]): void {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
 }
